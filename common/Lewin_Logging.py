45cb1fafe1d5105a6c0cf778a9c0fca31343ca00
Lewin <393070029@qq.com>
2019-04-23 18:36:36 +0800

    Rewrite class Lewin_Logging (only sys now); adding test for Lewin_Logging.

--- common/Lewin_Logging.py
+++ common/Lewin_Logging.py
@@ -7,12 +7,192 @@ __all__ = ["Lewin_Logging", "Easy_Logging", "Easy_Logging_Time", "Easy_Jack", "L
 
 """
 
-import os, sys, time, re
+import os, sys, time, re, _io
 from datetime import datetime, timedelta
+from typing import Union
+
+IP_Level_dic = {'all': 6, 'debug': 1, 'info': 2, 'warning': 3, 'error': 4, 'critical': 5, "none": 0}
+OP_Level_dic = {'all': 0, 'debug': 1, 'info': 2, 'warning': 3, 'error': 4, 'critical': 5, "none": 6}
+FMT = {'simple': "[%(levelname)s] %(message)s",
+       'info': "[%(levelname)s][%(time)s] %(message)s",
+       'all': "[%(levelname)s][%(time)s][%(filename)s %(funcName)s()][%(processid)s/%(lineno)d][%(loggername)s] %(message)s",
+       'debug': "[%(levelname)s][%(time)s][%(filename)s %(funcName)s()][%(processid)s/%(lineno)d] %(message)s",
+       'multiprocess': "[%(levelname)s][%(time)s][PID:%(processid)s] %(message)s"}
 
 
 # ————————————————————————————————————————————————————————
+def _translate_op_level(OP_level) -> int:
+    if isinstance(OP_level, str):
+        return OP_Level_dic[OP_level.lower()]
+    else:
+        return OP_level + 0
+
+
+class Lewin_Msg:
+    def __init__(self, IP_level_name: str, title, message, end):
+        dt = datetime.now()
+        self.levelname = IP_level_name.upper()
+        self.pathname = sys.argv[0]
+        self.filename = sys._getframe(2).f_code.co_filename
+        self.funcName = sys._getframe(2).f_code.co_name
+        self.processid = os.getpid()
+        self.lineno = sys._getframe(2).f_lineno
+        self.date = dt.strftime('%Y-%m-%d')
+        self.datetime = dt.strftime('%Y%m%d%H%M%S')
+        self.time = dt.strftime('%H:%M:%S')
+        self.message = message
+        self.IP_level = IP_Level_dic[IP_level_name]
+        self.title = title
+        self.end = end
+
+    def __str__(self) -> str:
+        return str(self.message) + str(self.end)
+
+    def translate(self, fmt: str):
+        if self.title is None:
+            return fmt % self.__dict__ + "%s" % self.end
+        else:
+            return "%s%s%s" % (self.title, self.message, self.end)
+
+
+class Lewin_LogMod_Jack:
+    def __init__(self, OP_level: Union[str, int] = "all", fmt: str = FMT['simple']):
+        self.kept_stdout = None
+        self.kept_stderr = None
+        self.OP_level = _translate_op_level(OP_level)
+        self.fmt = fmt
+
+    def ensure_sys(self):
+        assert isinstance(sys.stdout, _io.TextIOWrapper)
+        assert isinstance(sys.stderr, _io.TextIOWrapper)
+        return self
+
+    def kidnap_sys(self, logger, IP_level_name: str = "info"):
+        self.ensure_sys()
+        self.kept_stdout = sys.stdout
+        self.kept_stderr = sys.stderr
+        self.logger = logger
+        self.IP_level_name = IP_level_name
+        sys.stdout = self
+        sys.stderr = self
+
+    def release_sys(self):
+        sys.stdout = self.kept_stdout
+        sys.stderr = self.kept_stderr
+        self.kept_stdout = None
+        self.kept_stderr = None
+        self.logger = None
+        self.IP_level_name = None
+        self.ensure_sys()
+
+    def myclear(self):
+        if isinstance(sys.stdout, _io.TextIOWrapper):
+            pass
+        else:
+            self.release_sys()
+
+    # Output method
+    def mywrite(self, msg: Lewin_Msg):
+        if self.OP_level <= msg.IP_level:
+            message = msg.translate(self.fmt)
+            if self.kept_stdout:
+                self.kept_stdout.write(message)
+            else:
+                sys.stdout.write(message)
+
+    # Input methods
+    def write(self, message):
+        if message != "\n":  # print在输出之后会额外调用一次sys.stdout.write("\n")，而不是把\n加在输出的内容后面。
+            self.logger.mywrite(Lewin_Msg(self.IP_level_name, None, message, "\n"))
+
+    def flush(self):
+        pass
+
+
+
 class Lewin_Logging:
+    __date__ = "2019.04.23"
+
+    def __init__(self, name: str = None):
+        if name is None:
+            self._name = os.path.basename(sys._getframe(1).f_code.co_filename)
+        else:  # must be str.
+            self._name = name + ""
+        # Outputers
+        self.OPs = []
+
+    def __del__(self):
+        self.myclear()
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    # Safe mode -------------------------------
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.myclear()
+
+    # Hub Station -----------------------------
+    def mywrite(self, msg: Lewin_Msg) -> None:
+        for OP in self.OPs:
+            OP.mywrite(msg)
+
+    def myclear(self) -> None:
+        for OP in self.OPs:
+            try:
+                OP.myclear()
+            except Exception as e:
+                print(e)
+
+    # Input methods -----------------------------
+    def debug(self, message, title=None, end="\n"):
+        self.mywrite(Lewin_Msg("debug", title, message, end))
+
+    def info(self, message, title=None, end="\n"):
+        self.mywrite(Lewin_Msg("info", title, message, end))
+
+    def warning(self, message, title=None, end="\n"):
+        self.mywrite(Lewin_Msg("warning", title, message, end))
+
+    def error(self, message, title=None, end="\n"):
+        self.mywrite(Lewin_Msg("error", title, message, end))
+
+    def critical(self, message, title=None, end="\n"):
+        self.mywrite(Lewin_Msg("critical", title, message, end))
+
+    # Adding Outputers ------------------------------
+    def add_op_sys(self, OP_level: Union[str, int] = "all", jack: Lewin_LogMod_Jack = None):
+        for OP in self.OPs:
+            if isinstance(OP, Lewin_LogMod_Jack):
+                OP.OP_level = _translate_op_level(OP_level)
+                return self
+        else:
+            if jack:
+                self.OPs.append(jack)
+            else:
+                self.OPs.append(Lewin_LogMod_Jack(OP_level).ensure_sys())
+            return self
+
+    # Adding Inputers ------------------------------
+    def add_ip_sys(self, IP_level_name: str = "info", jack: Lewin_LogMod_Jack = None):
+        for OP in self.OPs:
+            if isinstance(OP, Lewin_LogMod_Jack):
+                OP.kidnap_sys(self, IP_level_name)
+                return self
+        else:
+            if jack:
+                self.OPs.append(jack)
+            else:
+                self.OPs.append(Lewin_LogMod_Jack(OP_level="none").ensure_sys())
+            return self
+
+
+
+# ————————————————————————————————————————————————————————
+class Lewin_Logging_old:
     level_dict = {'all': 0, 'debug': 1, 'info': 2, 'warning': 3, 'error': 4, 'critical': 5}
     fmt = {'simple': "[%(levelname)s] %(message)s",
            'info': "[%(levelname)s][%(time)s] %(message)s",
@@ -381,6 +561,7 @@ class Easy_Logging_Time:
 
 class Easy_Jack:
     """
+    Kidnap sys.stdout&sys.stderr, so is able to intercept prints.
     with Easy_Jack() as logger:
         # do something ...
         loggings = logger.text
